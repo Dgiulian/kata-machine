d52e472cd7aa8389f847cc0f4876d9d96c766d53
Diego Giuliani <giuliani.diego@gmail.com>
2023-07-06 08:55:14 -0300

    feat: two crystal balls

--- src/__tests__/TwoCrystalBalls.ts
+++ src/__tests__/TwoCrystalBalls.ts
@@ -1,14 +1,28 @@
 import two_crystal_balls from "@code/TwoCrystalBalls";
 
-test("two crystal balls", function () {
-    let idx = Math.floor(Math.random() * 10000);
-    const data = new Array(10000).fill(false);
+describe("two crystal balls", () => {
+    test("breaks at random position", () => {
+        let idx = Math.floor(Math.random() * 10000);
+        const data = new Array(10000).fill(false);
 
-    for (let i = idx; i < 10000; ++i) {
-        data[i] = true;
-    }
+        for (let i = idx; i < 10000; ++i) {
+            data[i] = true;
+        }
 
-    expect(two_crystal_balls(data)).toEqual(idx);
-    expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
+        expect(two_crystal_balls(data)).toEqual(idx);
+    });
+
+    test("it does not break", () => {
+        expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
+    });
+
+    test("it breaks at the first position", () => {
+        expect(two_crystal_balls(new Array(100).fill(true))).toEqual(0);
+    });
+    test("it breaks at the last position", () => {
+        let data = new Array(100).fill(false);
+        data[data.length - 1] = true;
+
+        expect(two_crystal_balls(data)).toEqual(99);
+    });
 });
-
---
+++ src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,19 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    let jmpAm = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jmpAm;
+    for (; i < breaks.length; i += jmpAm) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    // i -= jmpAm;
+
+    for (let j = i - jmpAm; j < i && j < breaks.length; ++j) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+    return -1;
+}
